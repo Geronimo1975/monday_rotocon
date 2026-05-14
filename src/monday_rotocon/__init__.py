b1ee6bdb815f08eedaf2410c5887c587e8c35ac0
ROTOCON_GEORGE <rotocondemo@mac.speedport.ip>
2026-05-14 22:19:06 +0200

    chore: bump to v0.2.1

--- src/monday_rotocon/__init__.py
+++ src/monday_rotocon/__init__.py
@@ -4,4 +4,4 @@ from monday_rotocon.models import Board, Column, ColumnValue, Item
 from monday_rotocon.transport import MondayAPIError, MondayClient
 
 __all__ = ["Board", "Column", "ColumnValue", "Item", "MondayAPIError", "MondayClient"]
-__version__ = "0.2.0"
+__version__ = "0.2.1"
